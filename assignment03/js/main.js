474f6c75c743964d7454c5653cb60601f118d6fc
Jennifer Presto <sandlappernyc@yahoo.com>
2014-02-13 22:08:21 -0500

    just the barest bones of an interface

--- assignment03/js/main.js
+++ assignment03/js/main.js
@@ -1,9 +1,11 @@
 // wrapper so entire page loads before js implemented
 window.onload = function () {
 
-	$(document).click(function(e) {
-		console.log("clicking!");
-		$('#container').removeClass('hide');
-	})
+	var sb;
+	var app_name = "testJGP";
+
+	function setup() {
+
+	}
 
 }
